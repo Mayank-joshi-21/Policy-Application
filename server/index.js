189ba8b27564b6f1aa7d43df1e99e2d68ee6dc7c
Mayank Joshi <113921344+Mayank-joshi-21@users.noreply.github.com>
2025-07-12 10:37:33 +0530

    remove auth

--- server/index.js
+++ server/index.js
@@ -3,21 +3,15 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
 app.use(express.json());
 
-// Routes
 const policyRouter = require('./routes/policy');
 app.use('/api/policy', policyRouter);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
